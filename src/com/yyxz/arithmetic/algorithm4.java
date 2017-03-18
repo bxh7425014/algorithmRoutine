ccd6c1715a6cf7efbf2a5d96f8de15b345fede21
bian.xh <bxh7425014@163.com>
2017-03-18 11:53:09 +0800

    分苹果问题。实质对应的数学中的计数原理中的组合理论。

--- src/com/yyxz/arithmetic/algorithm4.java
+++ src/com/yyxz/arithmetic/algorithm4.java
@@ -1,4 +1,7 @@
 package com.yyxz.arithmetic;
+
+import java.util.Scanner;
+
 /**
  * 
  * @author bianxh
@@ -12,5 +15,34 @@ package com.yyxz.arithmetic;
  * 样例输出:	8
  */
 public class algorithm4 {
-
+	public static void test() {
+		System.out.println("样例输入，比如\n7\n3:\n");
+		Scanner inputScanner = new Scanner(System.in);
+		int M = Integer.parseInt(inputScanner.nextLine());
+		int N = Integer.parseInt(inputScanner.nextLine());
+		inputScanner.close();
+		System.out.println("总共有 " + SharingApple(M,N) + " 种分法。");
+	}
+	
+	/**
+	 * 参考地址：http://www.th7.cn/Program/java/201407/235437.shtml
+	 * 题目分析：
+	 * 这道题类似于整数划分的题目，主要思路就是考递归。
+	 * 1、当苹果数目或者篮子数目为1时候，就只有一种可能
+	 * 2、当苹果数目小于篮子数目的时候，按照苹果数目来分配
+	 * 3、当苹果数目大于篮子数目的时候，空一个盘子 + 先每个盘子放一个之后再m-n个随便放
+	 * 4、当苹果数目等于篮子数目的时候，每个盘子放一个 + 空一个盘子
+	 * @param m
+	 * @param n
+	 * @return
+	 */
+	public static int SharingApple(int m, int n) {
+		if(m == 1 || n == 1)    
+			return 1;    
+		if(m < n)       
+			return SharingApple(m , m);    
+		else if(m > n)        
+			return SharingApple(m, n-1) + SharingApple(m-n, n); 
+		else        
+			return 1 + SharingApple(m, n-1);}
 }
--- src/com/yyxz/main/Main.java
+++ src/com/yyxz/main/Main.java
@@ -1,11 +1,11 @@
 package com.yyxz.main;
 
-import com.yyxz.arithmetic.algorithm2;
+import com.yyxz.arithmetic.algorithm4;
 
 public class Main {
 
 	public static void main(String[] args) {
-		algorithm2.test();
+		algorithm4.test();
 	}
 
 }
